a2797b8a66ceee068d9550a99e6dadf8f3ac11af
ionow-arteom <ionow.arteom@yandex.ru>
2023-08-30 13:45:47 +0600

    sp-16-1

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(Class<?> entityClass, String message) {
-        super("Сущность " + entityClass.getSimpleName() + " не найдена. " + message);
-    }
-}
